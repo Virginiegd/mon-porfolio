fb680c4ead9ea31dc0a2cad7b00108fde52f1beb
Virginiegd <111052171+Virginiegd@users.noreply.github.com>
2023-03-22 12:26:08 +0100

    début nav header

---
+++ mon-portfolio/src/components/Header.jsx
@@ -0,0 +1,32 @@
+import { NavLink } from "react-router-dom"
+
+function Header() {
+    let activeStyle = {
+        textDecoration: "underline"
+    }
+
+    return (
+        <header>
+            <div>
+                <div>
+                    <nav>
+                        <ul className="nav_ul">
+                            <li><NavLink className='nav__a' to="/" style={({ isActive }) =>
+                                isActive ? activeStyle : undefined} > Home </NavLink></li>
+                            <li><NavLink className='nav__a' to="/about" style={({ isActive }) =>
+                                isActive ? activeStyle : undefined} > About </NavLink></li>
+                            <li><NavLink className='nav__a' to="/skills" style={({ isActive }) =>
+                                isActive ? activeStyle : undefined} > Skills </NavLink></li>
+                            <li><NavLink className='nav__a' to="/portfolio" style={({ isActive }) =>
+                                isActive ? activeStyle : undefined} > Portfolio </NavLink></li>
+                            <li><NavLink className='nav__a' to="/contact" style={({ isActive }) =>
+                                isActive ? activeStyle : undefined} > Contact </NavLink></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    )
+    }
+    
+    export default Header
--- mon-portfolio/src/index.js
+++ mon-portfolio/src/index.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './styles/index.scss'
+import Home from './pages/Home'
+import About from './pages/About'
+import Skills from './pages/Skills'
+import Portfolio from './pages/Portfolio'
+import Error404 from './pages/Error404'
+import Contact from './pages/Contact'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/skills' element={<Skills />} />
+        <Route path='/portfolio' element={<Portfolio />} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='*' element={<Error404/>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-);
+)
+
