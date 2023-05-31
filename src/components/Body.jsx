dff6f7f9458b8afaf70844f1ca14b9a321ce1231
Virginiegd <111052171+Virginiegd@users.noreply.github.com>
2023-05-31 10:44:43 +0200

    modif page accueil + lien kasa

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,11 @@
-import photo1 from "../assets/photo1.jpg"
+import photo1 from "../assets/photo1.png"
 import html from "../assets/html.png"
 import css from "../assets/css.png"
 import sass from "../assets/sass.png"
 import js from "../assets/js.png"
 import react from "../assets/react.png"
 import nodejs from "../assets/node-js.png"
+import linkedin from "../assets/linkedin.png"
 
 function Body() {
 
@@ -16,21 +17,23 @@ function Body() {
                     <h1> Qui suis-je ? </h1>
                     <ul> 
                         <li>GIROUD Virginie </li>
-                        <li>Développeuse Web Fullstack</li>
-                        <li>37 ans</li>
+                        <li className="txt">37 ans</li>
+                        <li className="txt">Développeuse Web Fullstack</li>
+                        <li className="txt">RQTH/OETH</li>
+                        <li className="txtitalic"><img className="linkedin" src={linkedin} alt="linkedin"/><a href="https://www.linkedin.com/in/virginiegiroud/" target="blank">https://www.linkedin.com/in/virginiegiroud/</a></li>
                     </ul>
                 </div>
             </div>
             <div className="icones">
                 <ul className="languages">
-                    <li><img className="html" src={html} alt="HTML"/></li>
-                    <li><img className="css" src={css} alt="CSS"/></li>
-                    <li><img className="js" src={js} alt="Javascript"/></li>
+                    <li><img src={html} alt="HTML"/></li>
+                    <li><img src={css} alt="CSS"/></li>
+                    <li><img src={js} alt="Javascript"/></li>
                 </ul>
                 <ul className="languages">
-                    <li><img className="sass" src={sass} alt="Sass"/></li>
-                    <li><img className="nodejs" src={nodejs} alt="Node JS"/></li>
-                    <li><img className="react" src={react} alt="React"/></li>
+                    <li><img src={sass} alt="Sass"/></li>
+                    <li><img src={nodejs} alt="Node JS"/></li>
+                    <li><img src={react} alt="React"/></li>
                 </ul>
             </div>
         </div>
